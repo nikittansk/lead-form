7dbb2bc8d957a232a6e2adda53d9ff4dd153e014
Nikittansk <niknosokov@mail.ru>
2024-07-16 08:16:29 +0300

    fifth commit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,13 @@
 import { createApp } from 'vue';
-import App from './src/App.vue'
+import App from './src/App.vue';
 import router from './src/router/router.js';
+import axios from 'axios';
+window.axios = axios;
+
+
+window.axios.defaults.headers.common['X-CSRF-TOKEN'] = document.querySelector('meta[name="csrf-token"]')?.content;
 
 createApp(App)
     .use(router)
     .mount('#app');
+
